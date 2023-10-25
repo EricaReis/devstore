798ab048a463e5403b8342087ccd38923744c382
EricaReis <ericareis67@outlook.com>
2023-10-24 21:50:58 -0300

    Data Fetching on components

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,4 +1,17 @@
-export default function Catalog() {
+export default async function Catalog() {
+  //componente assíncrono quer dizer que o carregamento de dados pode ser
+  //diretamente dentro do componente
+
+  await new Promise(resolve => setTimeout(resolve, 2000))
+
+  //esse tipo de carregamento de dado só vai ser feito dessa maneira, quando o 
+  //componente precisar dessa informação pra ser renderizado assim que entrar
+  //na tela: (ou seja, não iremos usar quando precisar de uma ação do usuário
+  //para renderizar o dado, ex: botão)
+  const response = await fetch('https://api.github.com/users/EricaReis')
+  const user = await response.json()
+
   return (
-  <h1>Catalog</h1>  )
+    <pre>{JSON.stringify(user, null, 2)}</pre>
+  )
 }
