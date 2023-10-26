b5700c9bfd2d5cdb04eaf9a970d1abb3230fbe66
EricaReis <ericareis67@outlook.com>
2023-10-25 21:15:33 -0300

    Add basic devstore structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,3 @@
-import { Suspense } from 'react'
-import { GithubProfile } from '@/components/github-profile'
-import { LongWaitComponent } from '@/components/long-wait-component'
-
-// por padrão, o next aguarda TODOS os componentes finalizem de ser carregados
-// para mostrar o conteúdo.
-
-// Suspense API é um componente que pode ser usado em volta de um componente
-// que demora a ser carregado, isso faz com que possamos criar as sessões de loading
-// na página sem ser global, e sim para elementos separados
-
 export default async function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-
-      <Suspense fallback={<p>Carregando LongWaitComponent</p>}>
-        <LongWaitComponent />
-      </Suspense>
-
-      <Suspense fallback={<p>Carregando GithubProfile</p>}>
-        <GithubProfile />
-      </Suspense>
-    </div>
-  )
+  return <div>Hello world</div>
 }
