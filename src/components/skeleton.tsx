b0c9f536a9507b683193725a1e4e28b59111f505
EricaReis <ericareis67@outlook.com>
2023-10-31 22:58:40 -0300

    Add skeleton component resume 2

--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -1,6 +1,7 @@
 import { ComponentProps } from 'react'
 import { twMerge } from 'tailwind-merge'
 
+// div de skeleton que vai substituir todos os componentes durante o loading
 export function Skeleton({ className, ...props }: ComponentProps<'div'>) {
   return (
     <div
